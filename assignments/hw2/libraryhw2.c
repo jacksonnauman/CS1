dad2c391a6f7dd93ecd2694a617ee36d2a5a996c
JackNauman <jack.nauman@gmail.com>
2025-02-28 17:40:23 -0500

    hw2 done

--- assignments/hw2/libraryhw2.c
+++ assignments/hw2/libraryhw2.c
@@ -20,15 +20,14 @@ typedef struct node_t {
     struct node_t* prev;
 } node_t;
 
-node_t *borrowBook(node_t *head, char *title, char *lastName, char *firstName);
-node_t *returnBook(node_t *head, char *title, char *lastName, char *firstName);
-void checkBook(node_t *head, char *title, char *lastName, char *firstName);
-void displayBorrowedBooks(node_t *head);
-void freeList(node_t *head);
+node_t *borrowBook(node_t *head, char *title, char *lastName, char *firstName, FILE *output);
+node_t *returnBook(node_t *head, char *title, char *lastName, char *firstName, FILE *output);
+void checkBook(node_t *head, char *title, char *lastName, char *firstName, FILE *output);
+void displayBorrowedBooks(node_t *head, FILE *output);
+void freeList(node_t *head, FILE *output);
 
-node_t *borrowBook(node_t *head, char *title, char *lastName, char *firstName) {
+node_t *borrowBook(node_t *head, char *title, char *lastName, char *firstName, FILE *output) {
     // open file, create + allocate node and error check
-    FILE* file = fopen("output.txt", "a");
     node_t* borrow = malloc(sizeof(node_t));
     if (borrow == NULL) {
         printf("Malloc failure\n");
@@ -42,25 +41,22 @@ node_t *borrowBook(node_t *head, char *title, char *lastName, char *firstName) {
     if (head == NULL) {
         head = borrow;
         borrow->prev = NULL;
-        fprintf(file,"Borrowed \"%s\" by %s, %s\n", title, lastName, firstName);
-        fclose(file);
+        fprintf(output,"Borrowed \"%s\" by %s, %s\n", title, lastName, firstName);
         return borrow;
     }
     // otherwise insert node at head
     head->prev = borrow;
     borrow->next = head;
     borrow->prev = NULL;
-    fprintf(file,"Borrowed \"%s\" by %s, %s\n", title, lastName, firstName); // output to file
-    fclose(file);
+    fprintf(output,"Borrowed \"%s\" by %s, %s\n", title, lastName, firstName); // output to file
     return borrow;
 }
 
-node_t *returnBook(node_t *head, char *title, char *lastName, char *firstName) {
-    // open file, create temp node to traverse list, error check
-    FILE* file = fopen("output.txt", "a");
+node_t *returnBook(node_t *head, char *title, char *lastName, char *firstName, FILE *output) {
+    // create temp node to traverse list, error check
     node_t* temp = head;
     if (temp == NULL) {
-        fprintf(file,"\"%s\" is not currently borrowed by %s, %s\n", title, lastName, firstName);
+        fprintf(output,"\"%s\" is not currently borrowed by %s, %s\n", title, lastName, firstName);
         return NULL;
     }
     // set while loop conditions for readability
@@ -70,8 +66,8 @@ node_t *returnBook(node_t *head, char *title, char *lastName, char *firstName) {
     while (condition1 || condition2 || condition3) {
         temp = temp->next; // traverse linked list to find the correct node
         if (temp == NULL) {
-            fprintf(file,"\"%s\" is not currently borrowed by %s, %s\n", title, lastName, firstName);
-            return NULL; // if it ever hits this if statement, the book isnt in the list
+            fprintf(output,"\"%s\" is not currently borrowed by %s, %s\n", title, lastName, firstName);
+            return head; // if it ever hits this if statement, the book isnt in the list
         }
         // update the strcmps for next loop
         condition1 = strcmp(temp->title, title);
@@ -81,26 +77,23 @@ node_t *returnBook(node_t *head, char *title, char *lastName, char *firstName) {
     node_t* book = temp; // set the correct book bode
     if (book->prev == NULL) {
         head = book->next; // if correct book is the current head, head must be next node
-        fprintf(file, "Returned \"%s\" by %s, %s\n", book->title, book->lastName, book->firstName);
+        fprintf(output, "Returned \"%s\" by %s, %s\n", book->title, book->lastName, book->firstName);
         // finish out the function, freeing the node and closing file, returning the head
         free(book);
-        fclose(file);
         return head;
     }
     book->prev->next = book->next; // link the books previous node to the books next node, removing it
-    fprintf(file, "Returned \"%s\" by %s, %s\n", book->title, book->lastName, book->firstName); // output to file
+    fprintf(output, "Returned \"%s\" by %s, %s\n", book->title, book->lastName, book->firstName); // output to file
     // finish out the function, freeing the node and closing file, returning the head
     free(book);
-    fclose(file);
     return head;
 }
 
-void checkBook(node_t *head, char *title, char *lastName, char *firstName) {
-    // open file, create temp node to traverse list, error check
-    FILE* file = fopen("output.txt", "a");
+void checkBook(node_t *head, char *title, char *lastName, char *firstName, FILE *output) {
+    // create temp node to traverse list, error check
     node_t* temp = head;
     if (temp == NULL) {
-        fprintf(file,"\"%s\" is not currently borrowed by %s, %s\n", title, lastName, firstName);
+        fprintf(output,"\"%s\" is not currently borrowed by %s, %s\n", title, lastName, firstName);
         return;
     }
     // set while loop conditions for readability
@@ -110,8 +103,7 @@ void checkBook(node_t *head, char *title, char *lastName, char *firstName) {
     while (condition1 || condition2 || condition3) {
         temp = temp->next;
         if (temp == NULL) {
-            fprintf(file,"\"%s\" is not currently borrowed by %s, %s\n", title, lastName, firstName);
-            fclose(file);
+            fprintf(output,"\"%s\" is not currently borrowed by %s, %s\n", title, lastName, firstName);
             return; // if this if statement happens the book is not in the list
         }
         // update strcmps for next loop
@@ -120,30 +112,30 @@ void checkBook(node_t *head, char *title, char *lastName, char *firstName) {
         condition3 = strcmp(temp->firstName, firstName);
     }
     // otherwise, output to file and close file
-    fprintf(file, "\"%s\" is currently checked out by %s, %s\n", title, lastName, firstName);
-    fclose(file);
+    fprintf(output, "\"%s\" is currently checked out by %s, %s\n", title, lastName, firstName);
 }
 
-void displayBorrowedBooks(node_t *head) {
+void displayBorrowedBooks(node_t *head, FILE *output) {
     // open file, create temp node to traverse list, error check
-    FILE* file = fopen("output.txt", "a");
     node_t *temp = head;
     if (temp == NULL) {
-        fprintf(file,"There are currently no borrowed books\n");
+        fprintf(output, "There are currently no borrowed books\n");
         return;
     }
     int increment = 0; // for numbering in list
-    fprintf(file,"Borrowed books:\n");
+    fprintf(output,"Borrowed Books List:\n");
     // this while loop just prints out each node line by line
-    while (temp != NULL) {
-        increment++;
-        fprintf(file,"%d. \"%s\" - %s, %s\n", increment, temp->title, temp->lastName, temp->firstName);
+    while (temp->next != NULL) {
         temp = temp->next;
     }
-    fclose(file); // close file
+    while (temp != NULL) {
+        increment++;
+        fprintf(output,"%d. \"%s\" - %s, %s\n", increment, temp->title, temp->lastName, temp->firstName);
+        temp = temp->prev;
+    }
 }
 
-void freeList(node_t *head) {
+void freeList(node_t *head, FILE *output) {
    // simply traverse list and free node by node
     while (head != NULL) {
         node_t *temp = head;
@@ -158,37 +150,37 @@ int main(void) {
     node_t* head = NULL;
     char firstName[MAX_NAME], lastName[MAX_NAME], title[MAX_TITLE], buffer[100];
     // open the files and error check
-    FILE* inputFile = fopen("library_records.txt", "r");
-    FILE* outputFile = fopen("output.txt", "a");
-    if (inputFile == NULL) {
+    FILE* input = fopen("library_records.txt", "r");
+    FILE* output = fopen("output.txt", "w");
+    if (input == NULL) {
         printf("File does not exist\n");
         return -1;
     }
     // run loop line by line until input 5 to exit
-    while (operator != 5 && fgets(buffer, sizeof(buffer), inputFile) != NULL) {
+    while (operator != 5 && fgets(buffer, sizeof(buffer), input) != NULL) {
         // parse buffer for data
         sscanf(buffer, "%d \"%[^\"]\" %s %s", &operator, title, firstName, lastName);
         // switch statement assigns operator to functions
         switch (operator) {
             case 1:
-                head = borrowBook(head, title, lastName, firstName);
+                head = borrowBook(head, title, lastName, firstName, output);
                 break;
             case 2:
-                head = returnBook(head, title, lastName, firstName);
+                head = returnBook(head, title, lastName, firstName, output);
                 break;
             case 3:
-                checkBook(head, title, lastName, firstName);
+                checkBook(head, title, lastName, firstName, output);
                 break;
             case 4:
-                displayBorrowedBooks(head);
+                displayBorrowedBooks(head, output);
                 break;
             default:
-                fprintf(outputFile, "Thank you for using the Library System!\n");
+                fprintf(output, "Thank you for using the Library System!\n");
         }
     }
     // free list, close files, return 0
-    freeList(head);
-    fclose(inputFile);
-    fclose(outputFile);
+    freeList(head, output);
+    fclose(input);
+    fclose(output);
     return 0;
 }
